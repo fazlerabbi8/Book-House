f37d3c37f0ad7c129fadd834bec1b53321de9fa8
fazlerabbi13 <mohammad0132794@gmail.com>
2026-07-23 16:28:42 +0600

    added sort button in listed books section.

--- src/pages/Listed.jsx
+++ src/pages/Listed.jsx
@@ -5,36 +5,61 @@ import "react-tabs/style/react-tabs.css";
 import { getStoredReadList, getWishList } from "../utility/add";
 import Book from "../components/Book/Book";
 const Listed = () => {
-    const [readList, setReadList] = useState([]);
-    const [wishList, setWishList] = useState([]);
+  const [readList, setReadList] = useState([]);
+  const [wishList, setWishList] = useState([]);
 
-    const allBooks = useLoaderData();
+  const allBooks = useLoaderData();
 
-    useEffect(()=>{
-        const storedReadList = getStoredReadList();
+  useEffect(() => {
+    const storedReadList = getStoredReadList();
 
-       const storedReadListInt = storedReadList.map(id =>parseInt(id));
+    const storedReadListInt = storedReadList.map((id) => parseInt(id));
 
-       const readList = allBooks.filter(book => storedReadListInt.includes(book.bookId));
+    const readList = allBooks.filter((book) =>
+      storedReadListInt.includes(book.bookId),
+    );
 
-       // eslint-disable-next-line react-hooks/set-state-in-effect
-       setReadList(readList);
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    setReadList(readList);
+  }, []);
 
-    },[])
+  useEffect(() => {
+    const storedWishList = getWishList();
 
-    useEffect(() =>{
-        const storedWishList = getWishList();
+    const wishListInt = storedWishList.map((id) => parseInt(id));
 
-        const wishListInt = storedWishList.map(id =>parseInt(id));
+    const wishList = allBooks.filter((book) =>
+      wishListInt.includes(book.bookId),
+    );
 
-        const wishList = allBooks.filter(book => wishListInt.includes(book.bookId));
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    setWishList(wishList);
+  }, []);
 
-        // eslint-disable-next-line react-hooks/set-state-in-effect
-        setWishList(wishList);
-    },[])
   return (
     <div>
       <h3 className="text-3xl font-bold text-center">Listed Books</h3>
+
+      <div className="text-center">
+        <div className="dropdown">
+          <div>
+            <div tabIndex={0}       role="button" className="btn btn-accent m-1 mt-5">
+              Sort By
+            </div>
+          </div>
+          <ul
+            tabIndex="-1"
+            className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm"
+          >
+            <li>
+              <a>Rating</a>
+            </li>
+            <li>
+              <a>Number of pages</a>
+            </li>
+          </ul>
+        </div>
+      </div>
 
       <div className="mt-5">
         <Tabs>
@@ -44,24 +69,26 @@ const Listed = () => {
           </TabList>
 
           <TabPanel>
-            <h2 className="text-2xl font-bold mb-10">Book I Read: {readList.length}</h2>
+            <h2 className="text-2xl font-bold mb-10">
+              Book I Read: {readList.length}
+            </h2>
 
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 place-items-center">
-                {
-                    readList.map(book => <Book key={book.bookId} book={book}></Book>)
-                }
+              {readList.map((book) => (
+                <Book key={book.bookId} book={book}></Book>
+              ))}
             </div>
           </TabPanel>
           <TabPanel>
-            <h2 className="text-2xl font-bold mb-10">My Wish List: {wishList.length}</h2>
+            <h2 className="text-2xl font-bold mb-10">
+              My Wish List: {wishList.length}
+            </h2>
 
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 place-items-center">
-                {
-                    wishList.map(book => <Book key={book.bookId} book={book}></Book>)
-                }
+              {wishList.map((book) => (
+                <Book key={book.bookId} book={book}></Book>
+              ))}
             </div>
-
-
           </TabPanel>
         </Tabs>
       </div>
